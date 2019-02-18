522861918713d3b66d6ad75a096d7facb76e55f1
waldganger <antony.merle@yahoo.fr>
2019-02-18 21:58:17 +0100

    ajout commentaires dans code

--- opus.sh
+++ opus.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-OIFS="$IFS"
-IFS=$(\s\S)
+OIFS="$IFS" #évite que les espaces dans les boucles génèrent des erreurs
+IFS=$(\s\S) #évite que les espaces dans les boucles génèrent des erreurs
 for i in *.flac
     do ffmpeg -i $i -acodec libopus -b:a 128000 $i.opus
 done
-dir=${PWD##*/}
+dir=${PWD##*/} #assigne le nom du répertoire à une variable
 mkdir "$dir"
 for o in *.opus
     do mv $o "$dir"/
 done
-IFS=$SAVEIFS
+IFS=$SAVEIFS #évite que les espaces dans les boucles génèrent des erreurs
